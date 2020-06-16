c778f493c57d987795cda75a5863587e7362072b
cshar <icetom6@hotmail.com>
2020-06-15 21:05:41 -0500

    lg2

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,27 @@
 import React,{Component} from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/home'
+import Login from './components/login'
+import Dashboard from './components/dashboard'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      authed: true
+    }
+  }
+
+
   render(){
     return(
-      <div>
-      <h1> Hello world </h1>
-      </div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+        </Router> 
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.json());
 
+//StaticFiles
+app.use(express.static(path.join(__dirname,'public')))
+
 //Routes
 app.use(require('./routes/rutas'));
 
-//StaticFiles
-app.use(express.static(path.join(__dirname,'public')))
+
+
 
 app.listen(app.get('port'),()=>{
   console.log(`server on port ${app.get('port')}`)
