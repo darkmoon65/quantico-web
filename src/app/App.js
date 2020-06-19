7d95c8f1863ddf60488cfeb7a927fa8c02c35973
cesar_dev <ice665@hotmail.com>
2020-06-18 21:58:11 -0500

    act-l

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/home'
 import Login from './components/login'
 import Dashboard from './components/dashboard'
+import Users from './components/dashComponents/users'
+import Configuracion from './components/dashComponents/config'
+import Comprobantes from './components/dashComponents/comprobant'
+import BlackList from './components/dashComponents/blacklist'
 
 class App extends Component {
 
@@ -20,8 +24,21 @@ class App extends Component {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/login' component={Login} />
+          <Route
+                path="/dashboard"
+                render={({ match: { url } }) => (
+                    <>
+                      <Route path={`${url}/`} component={Dashboard} exact />
+                      <Route path={`${url}/users`} component={Users} />
+                      <Route path={`${url}/blacklist`} component={BlackList} />
+                      <Route path={`${url}/configuracion`} component={Configuracion} />
+                      <Route path={`${url}/comprobantes`} component={Comprobantes} />
+
+                    </>
+                  )}
+          />
         </Switch>
-        </Router> 
+      </Router>
     )
   }
 }
--- src/app/components/login.js
+++ src/app/components/login.js
@@ -1,13 +1,89 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class Login extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      redirect: false,
+      correo: '',
+      contraseña: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.ingresar = this.ingresar.bind(this);
+  }
+  handleChange(e){
+      const {name, value} = e.target;
+
+      this.setState({
+        name: value
+      })
+      console.log(name)
+      console.log(value)
+      //this.fetchQuery(value,name);
+      //console.log(name);
+      }
+
+  ingresar(){
+    fetch('http://107.23.50.10/loginAdmin',{
+            method: 'POST',
+            body: JSON.stringify({
+                correo: this.state.correo,
+                contrasena: this.state.contraseña
+            }),
+            headers: {
+                'Accept' : 'application/json',
+                'Content-type' : 'application/json'
+            }
+          }).then(res =>res.json())
+          .then(data => {
+            if(data.response==true){
+              console.log(data)
+              this.setState({redirect:true })
+            }
+            else{
+            }
+        }).catch((error)=> {
+          console.log('Hubo un problema con la petición Fetch:' + error.message);
+      });
+  }
+
   render(){
+
+    const { redirect } = this.state;
+
+     if (redirect) {
+       return <Redirect to='/dashboard'/>;
+     }
     return(
-      
       <div>
-          hola Login
-      </div>
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container">
+            <Link to="/" className="navbar-brand" >Quantico </Link>
+          </div>
+        </nav>
+
+
+        <div className="container p-5 ">
+            <div className="row justify-content-md-center">
+                <div className="col-md-4" >
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="form-group">
+                        <input type="text" className="form-control" name="correo" placeholder="Usuario" onChange={this.handleChange}/>
+                      </div>
+                      <div className="form-group">
+                        <input type="password" className="form-control" name="contraseña" placeholder="Contraseña" onChange={this.handleChange} />
+                      </div>
+                      <input type="button" className="btn btn-success btn-block" onClick={()=>this.ingresar()} value="Ingresar" />
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
     )
   }
 }
