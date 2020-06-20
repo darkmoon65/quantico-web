fa425bd65a1c3118de98d3ac2c0efbbbc322c41e
Nakama-YefrixD <43789916+Nakama-YefrixD@users.noreply.github.com>
2020-06-20 00:04:11 -0500

    migrar template

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,52 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './App';
+import React, { Component, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
-render(<App/>, document.getElementById('app'));
+import '../../node_modules/font-awesome/scss/font-awesome.scss';
+
+import Loader from './layout/Loader'
+import Aux from "../hoc/_Aux";
+import ScrollToTop from './layout/ScrollToTop';
+import routes from "../route";
+import IndexLogin from '../sistema/login/index';
+
+const AdminLayout = Loadable({
+    loader: () => import('./layout/AdminLayout'),
+    loading: Loader
+});
+
+console.log(localStorage.getItem('token'));
+class App extends Component {
+    
+    render() {
+        const menu = routes.map((route, index) => {
+          return (route.component) ? (
+              <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  name={route.name}
+                  render={props => (
+                      <route.component {...props} />
+                  )} />
+          ) : (null);
+        });
+
+        return (
+            localStorage.getItem('token') === "none"
+            ?<IndexLogin/>
+            :<Aux>
+                <ScrollToTop>
+                    <Suspense fallback={<Loader/>}>
+                        <Switch>
+                            {menu}
+                            <Route path="/" component={AdminLayout} />
+                        </Switch>
+                    </Suspense>
+                </ScrollToTop>
+            </Aux>
+        );
+    }
+}
+
+export default App;
---
+++ src/menu-items.js
@@ -0,0 +1,61 @@
+export default {
+    items: [
+        {
+            id: 'quiantico',
+            title: 'Quantico',
+            type: 'group',
+            icon: 'icon-navigation',
+            children: [
+                // {
+                //     id: 'login',
+                //     title: 'Login',
+                //     type: 'item',
+                //     url: '/login',
+                //     icon: 'feather icon-home',
+                // },
+                {
+                    id: 'usuarios',
+                    title: 'Usuarios',
+                    type: 'item',
+                    url: '/usuarios',
+                    icon: 'feather icon-home',
+                },
+                {
+                    id: 'slider',
+                    title: 'Slider',
+                    type: 'item',
+                    url: '/slider',
+                    icon: 'feather icon-home',
+                },
+                {
+                    id: 'membresias',
+                    title: 'Membresias',
+                    type: 'item',
+                    url: '/membresias',
+                    icon: 'feather icon-home',
+                },
+                {
+                    id: 'productos',
+                    title: 'Productos',
+                    type: 'item',
+                    url: '/productos',
+                    icon: 'feather icon-home',
+                },
+                {
+                    id: 'verificaciones',
+                    title: 'Verificaciones',
+                    type: 'item',
+                    url: '/verificaciones',
+                    icon: 'feather icon-home',
+                },
+                {
+                    id: 'compras',
+                    title: 'Compras',
+                    type: 'item',
+                    url: '/compras',
+                    icon: 'feather icon-shopping-cart',
+                },
+            ]
+        },
+    ]
+}
