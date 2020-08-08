6f33496895ee71e0cdc969b1f05c80f1b8271709
Nakama-YefrixD <43789916+Nakama-YefrixD@users.noreply.github.com>
2020-08-07 22:20:40 -0500

    ver notificaciones

--- src/sistema/notificaciones/index.js
+++ src/sistema/notificaciones/index.js
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
-import {Row, Col, Card, Table, Modal, Form, InputGroup, FormControl, Button} from 'react-bootstrap';
+import {Row, Col, Card, Table, Modal, Form, InputGroup, Pagination, Button} from 'react-bootstrap';
 import Aux from "../../hoc/_Aux";
 import cogoToast from "cogo-toast";
 import Config from "../../config"
 import Files from "../../files"
+import Paginar from "../../paginate"
 
 class IndexComprobantes extends Component {
 
     constructor(){
         super();
         this.state = {
-            tb_notificaciones:[],
+            tb_notificaciones:{"data" : []},
             boolModal:false,
-            txt_crearNotificacion_mensaje : ''
+            tb_membresias             : [],
+            selec_crearNotificacion_enviara   : true,
+            selec_crearNotificacion_membresia : '',
+            txt_crearNotificacion_titulo      : '',
+            txt_crearNotificacion_mensaje     : '',
+            txt_texto_numeroPagina :1,
+
         }
         this.handleChange = this.handleChange.bind(this);
+        this.fetchMembresias = this.fetchMembresias.bind(this);
+        this.fetchNotificaciones = this.fetchNotificaciones.bind(this);
     }
+    
     descargarExcel(){
       Files.exportToCSV(this.state.tb_notificaciones,"notificaciones");
     }
-    fetchNotificaciones(){
-        fetch(`${Config.api}notificaciones/mostrar`,
+
+    fetchNotificaciones(bolean, numero){
+        fetch(`${Config.api}notificaciones/mostrar?page=`+numero,
           {
             mode:'cors',
             method: 'GET',
@@ -51,9 +62,51 @@ class IndexComprobantes extends Component {
         });
     }
 
+    fetchMembresias(){
+        fetch(`${Config.api}membresia/mostrar`,
+          {
+            mode:'cors',
+            method: 'GET',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-type' : 'application/json',
+                'api_token': localStorage.getItem('token')
+            }
+          }
+        )
+        .then(res =>res.json())
+        .then(data => {
+            if(data.respuesta === true){
+                this.setState({
+                    tb_membresias: data
+                });
+    
+                
+                let newArray = []
+                this.state.tb_membresias.datos.map((data)=>{
+                    newArray.push({
+                        id           : data.id,
+                        nombre       : data.nombre,
+                        seleccionado : true
+                    })
+                })
+                this.state.tb_membresias.datos = newArray;
+                this.state.selec_crearNotificacion_membresia = newArray[0]['id'];
+                this.setState({
+                    tb_membresias: this.state.tb_membresias
+                })
+            }
+            else{
+                
+            }
+        }).catch((error)=> {
+            console.log('Hubo un problema con la petición Fetch:' + error.message);
+        });
+    }
+
     componentDidMount(){
-      this.fetchNotificaciones();
-      console.log(localStorage.getItem('token'));
+      this.fetchNotificaciones(true, this.state.txt_texto_numeroPagina);
+      this.fetchMembresias();
     }
 
     agregarNotificacion(){
@@ -62,9 +115,18 @@ class IndexComprobantes extends Component {
             mode:'cors',
             method: 'POST',
             body: JSON.stringify({
-                todos     : true,
-                idUsuario : null,
-                mensaje   : this.state.txt_crearNotificacion_mensaje,
+                // todos     : true,
+                // idUsuario : null,
+                // mensaje   : this.state.txt_crearNotificacion_mensaje,
+
+                todos       : this.state.selec_crearNotificacion_enviara,
+                token       : null,
+                idUsuario   : null,
+                idMembresia : this.state.selec_crearNotificacion_membresia,
+                titulo      : this.state.txt_crearNotificacion_titulo,
+                mensaje     : this.state.txt_crearNotificacion_mensaje,
+                codigo      : null,
+                vista       : null,
               }
             ),
             headers: {
@@ -79,7 +141,7 @@ class IndexComprobantes extends Component {
             if(data.respuesta === true){
                 cogoToast.success("Notificacion enviada");
                 this.abrirModal();
-                this.fetchNotificaciones();
+                this.fetchNotificaciones(true, this.state.txt_texto_numeroPagina);
             }
             else{
               console.log(data)
@@ -98,10 +160,25 @@ class IndexComprobantes extends Component {
     }
 
     handleChange(e){
-        const {name, value} = e.target;
+        let {name, value} = e.target;
+        if(name == 'selec_crearNotificacion_enviara')
+        {
+            if(value == 'todos')
+            {
+                value = true
+            }else{
+                value = false
+            }
+        }
+
         this.setState({
           [name]: value
         })
+    }
+
+    fetchObtenerUsuario()
+    {
+
     }
 
     render() {
@@ -135,7 +212,7 @@ class IndexComprobantes extends Component {
                                     <tbody>
                                     {
                                         this.state.tb_notificaciones
-                                        ?this.state.tb_notificaciones.map((task, posicion) => {
+                                        ?this.state.tb_notificaciones.data.map((task, posicion) => {
                                             return (
                                                 <tr key={posicion}>
                                                     <td>{posicion+1}</td>
@@ -152,6 +229,28 @@ class IndexComprobantes extends Component {
                                     }
                                     </tbody>
                                 </Table>
+                                <Pagination>
+                                    <Pagination.Prev 
+                                    onClick={() => this.fetchNotificaciones(
+                                        true,
+                                        parseInt(this.state.txt_texto_numeroPagina)-1,
+                                    )}
+                                    />
+                                    {
+                                    this.state.tb_notificaciones
+                                    ?<Paginar 
+                                        data  = { this.state.tb_notificaciones} 
+                                        fetch = { (bolean,numero)=>this.fetchNotificaciones(bolean,numero)} >
+                                    </Paginar>
+                                    :null
+                                    }
+                                    <Pagination.Next
+                                    onClick={() => this.fetchNotificaciones(
+                                        true,
+                                        parseInt(this.state.txt_texto_numeroPagina)+1,
+                                    )}
+                                    />
+                                </Pagination>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -169,11 +268,75 @@ class IndexComprobantes extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <Row>
-                            <Col>
-                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Col md={12}>
+                                <Form.Group controlId="select.enviara">
+                                    <Form.Label>ENVIAR A:</Form.Label>
+                                    <Form.Control as="select" className="mb-3" name="selec_crearNotificacion_enviara"  onChange={this.handleChange}>
+                                        <option value={"todos"} >TODOS</option>
+                                        {/* <option value={"usuario"} >USUARIO</option> */}
+                                        <option value={"membresia"} >MEMBRESÍA</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                {/* <Form.Group controlId="input.ntarjetaNotificacion">
+                                    <Form.Label>N° TARJETA</Form.Label>
+                                    <Form.Control name="txt_crearNotificacion_ntarjeta"  onChange={this.handleChange} />
+                                </Form.Group> */}
+                                {/* <Row>
+                                    <Col md={6}>
+                                        <Form.Group controlId="input.correoNotificacion">
+                                            <Form.Label>CORREO</Form.Label>
+                                            <Form.Control name="txt_crearNotificacion_correo"  onChange={this.handleChange} disabled />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col md={6}>
+                                        <Form.Group controlId="input.nombreUsuarioNotificacion">
+                                            <Form.Label>NOMBRE</Form.Label>
+                                            <Form.Control name="txt_crearNotificacion_nombre"  onChange={this.handleChange} disabled/>
+                                        </Form.Group>
+                                    </Col>
+                                </Row> */}
+                                {
+                                    this.state.selec_crearNotificacion_enviara == false
+                                    ?<Form.Group controlId="select.membresia">
+                                        <Form.Label>SELECCIONAR MEMBRESÍA:</Form.Label>
+                                        <Form.Control as="select" className="mb-3" name="selec_crearNotificacion_membresia"  onChange={this.handleChange}>
+                                            {
+                                                this.state.tb_membresias.datos
+                                                ?this.state.tb_membresias.datos.map((task, posicion) => {
+                                                    return (
+                                                        <option 
+                                                            key   = {posicion}
+                                                            value = {task.id} 
+                                                        >
+                                                            {task.nombre}
+                                                        </option>
+                                                        // <Form.Check 
+                                                        //     key      = {posicion}
+                                                        //     type    = "checkbox" 
+                                                        //     name     = {posicion} 
+                                                        //     onChange = {this.obtenerCambioInputMembresias}
+                                                        //     checked = {task.seleccionado}
+                                                        //     label   = {task.nombre}
+                                                        // />
+                                                    );
+                                                })
+                                                :null
+                                            }
+                                        </Form.Control>
+                                    </Form.Group>
+                                    :null
+                                }
+                                <hr/>
+                                <Form.Group controlId="input.tituloNotificacion">
+                                    <Form.Label>TÍTULO</Form.Label>
+                                    <Form.Control name="txt_crearNotificacion_titulo" onChange={this.handleChange} />
+                                </Form.Group>
+                                <Form.Group controlId="input.mensajeNotificacion">
                                     <Form.Label>Mensaje Notificación</Form.Label>
                                     <Form.Control name="txt_crearNotificacion_mensaje" as="textarea" rows="3" onChange={this.handleChange} />
                                 </Form.Group>
+                            </Col>
+                            <Col>
                                 <InputGroup className="mb-12">
                                     <Button
                                         onClick={()=>{this.agregarNotificacion();}}
